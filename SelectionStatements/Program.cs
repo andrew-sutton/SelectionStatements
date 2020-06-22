34f32105653f3326f06d5712dd19459f3f2c5bae
Andrew Sutton <ajsutton94@gmail.com>
2020-06-22 15:25:23 -0500

    first commit, added while loop to assignment

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            var userInput = -1;
+
+            Console.WriteLine("Guess my favorite number:");
+            while (true)
+            {
+                userInput = int.Parse(Console.ReadLine());
+                if (userInput == favNumber)
+                {
+                    Console.WriteLine("Correct!");
+                    break;
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else if (userInput < favNumber)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else { Console.WriteLine("Nevermind"); }
+            }
+        }
+    }
+}
