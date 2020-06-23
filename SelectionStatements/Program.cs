402bee7facb4ebecb66d73b2b09b74c1b248c2dd
Andrew Sutton <ajsutton94@gmail.com>
2020-06-23 09:45:17 -0500

    added the switch statement from the second part of the assignment

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -29,6 +29,32 @@ namespace SelectionStatements
                 }
                 else { Console.WriteLine("Nevermind"); }
             }
+            // Second part of the assignmnet, asking about school subjects
+            Console.WriteLine("What is your favorite school subject?");
+
+            var favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject)
+            {
+                case "Math":
+                    Console.WriteLine("The highest form of pure thought is in mathematics - Plato");
+                    break;
+                case "Social Studies":
+                    Console.WriteLine("Do you like people?");
+                    break;
+                case "Gym":
+                    Console.WriteLine("I think I see a proffessional athele");
+                    break;
+                case "History":
+                    Console.WriteLine("The more you know about the past, the better prepared you are for the future");
+                    break;
+                case "Art":
+                    Console.WriteLine("The art of a people is a true mirror into their minds");
+                    break;
+                default:
+                    Console.WriteLine("I haven't taken that subject before");
+                    break;
+            }
         }
     }
 }
